9df2e7ca7cb1c28515b78a4b794f15502f4080b7
krrishmahar <krrishmahar5@gmail.com>
2024-07-14 00:32:20 +0530

    created mailing app

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify
+from smtplib import SMTPException
+from flask_mail import Message
+from app import app, mail
+
+bp = Blueprint('routes', __name__)
+
+@app.route('/')
+def publish():
+    try:
+        with mail.connect() as conn:
+            msg = Message(
+                '[Microblog] Test case',
+                sender=app.config["MAIL_SENDER"],
+                recipients=['test-fi3ylmc1j@srv1.mail-tester.com'],
+                body="This is the plain text version of the email. It is important to have a plain text version for better deliverability.",
+                html="""
+                <html>
+                    <body>
+                        This is the <b>HTML</b> body.<br>
+                        <p>Thank you for using our service!</p>
+                    </body>
+                </html>
+                """
+            )
+            conn.send(msg)
+        return jsonify({'message': 'Your message has been sent successfully'}), 200
+    except SMTPException as e:
+        print(f"SMTP error occurred: {e}")
+        return jsonify({'message': 'Failed to send email due to SMTP error'}), 500
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return jsonify({'message': 'Failed to send email due to an unexpected error'}), 500
